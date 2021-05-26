adfd95439bd2ddcd363336a6767e420d29895c07
shakuneko47 <shakuneko47@gmail.com>
2021-05-26 16:02:38 +0800

    0526

--- src/api/index.js.js
+++ src/api/index.js.js
@@ -9,12 +9,15 @@ import designers from "../json/designers.json";
 import aboutus from "../json/about-us";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEUGQwE2ts9kDMgeCXcd12x8M5KBVBIss",
-  authDomain: "yourhome2-37337.firebaseapp.com",
-  projectId: "yourhome2-37337",
-  storageBucket: "yourhome2-37337.appspot.com",
-  messagingSenderId: "199138331232",
-  appId: "1:199138331232:web:c6c36292a4d547fd085105"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+
 };
 
 firebase.initializeApp(firebaseConfig);
